0efec1735ae1de1a937ed19ebc38efb9107fa652
Moonhyeseong <moonhs97@gmail.com>
2021-11-08 16:32:43 +0900

    1.0

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Lap-Top SHOP</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="jumbotron">
+        <div className="text-area">
+          <h1 className="display-4">Hello, Lap-Top!</h1>
+          <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <p className="lead">
+            <a className="btn btn-primary btn-lg" href="#" role="button">
+              Learn more
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            <h4>상품명</h4>
+            <p>상품가격 & 설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
+            <h4>상품명</h4>
+            <p>상품가격 & 설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
+            <h4>상품명</h4>
+            <p>상품가격 & 설명</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
