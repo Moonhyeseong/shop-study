d774cdfbb03bc997f080221c4b667d2176e54ee5
Moonhyeseong <moonhs97@gmail.com>
2021-11-09 11:00:46 +0900

    1.1

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
 
       <div className="jumbotron">
         <div className="text-area">
-          <h1 className="display-4">Hello, Lap-Top!</h1>
+          <h1 className="display-4">Hello!</h1>
           <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
 
           <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
