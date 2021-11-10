5e638026f1d46543a8adb068c24208d2e2518b5e
Moonhyeseong <moonhs97@gmail.com>
2021-11-10 17:04:48 +0900

    Router-1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+/*eslint-disable*/
+import React, { useState } from 'react';
 import './App.css';
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import shoesData from './data.js';
+import Card from './components/card.js';
+
+import { Link, Switch, Route } from 'react-router-dom';
 function App() {
+  let [shoes, setShoes] = useState(shoesData);
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
@@ -22,39 +30,46 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      <Route exact path="/">
+        <div className="jumbotron">
+          <div className="text-area">
+            <h1 className="display-4">20% Season OFF</h1>
+            <p className="lead">
+              This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
+            </p>
 
-      <div className="jumbotron">
-        <div className="text-area">
-          <h1 className="display-4">Hello!</h1>
-          <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-
-          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-          <p className="lead">
-            <a className="btn btn-primary btn-lg" href="#" role="button">
-              Learn more
-            </a>
-          </p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            <h4>상품명</h4>
-            <p>상품가격 & 설명</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
-            <h4>상품명</h4>
-            <p>상품가격 & 설명</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
-            <h4>상품명</h4>
-            <p>상품가격 & 설명</p>
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <p className="lead">
+              <a className="btn btn-primary btn-lg" href="#" role="button">
+                Learn more
+              </a>
+            </p>
           </div>
         </div>
-      </div>
+
+        <div className="container">
+          <div className="row">
+            {shoes.map((data, index) => {
+              return <Card shoes={data} index={index} key={index} />;
+            })}
+          </div>
+        </div>
+      </Route>
+      <Route path="/detail">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className="btn btn-danger">주문하기</button>
+            </div>
+          </div>
+        </div>{' '}
+      </Route>
     </div>
   );
 }
